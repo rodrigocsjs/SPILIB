d2897fb42139a683c6d47c074c276edf00ca6a5d
rodrigocsjs <rodrigocsjs@gmail.com>
2022-10-25 12:48:03 -0300

    Add files via upload

---
+++ SPI.cpp
@@ -0,0 +1,136 @@
+/*
+ * SPI.cpp
+ *
+ * Created: 09/08/2022 02:45:02
+ *  Author: Rodrigo
+ */ 
+
+
+
+
+
+
+#include "SPI.h"
+
+#include "../SERIAL/SERIAL.h"
+
+
+
+static  unsigned char SLAVEDADOTX[32],SLAVEDADORX[32],SLAVEDADOIDRX =0X00,SLAVEDADOIDTX =0X00, SLAVERECE =0X00,SLAVEENVI =0X00;
+
+static void (*SLAVERECEBE)(unsigned char*, unsigned char);
+
+
+//================================================================================
+void Slave_RxTx_spi( void (*function)(unsigned char*, unsigned char) )
+{
+	SLAVERECE =1;
+	SLAVEDADOIDRX =0X00;
+	SLAVEENVI =0X00;
+	SLAVERECEBE = function;	
+}
+//================================================================================
+void Slave_Envia_spi(const unsigned char *dado, unsigned char qtd)
+{
+	SLAVEENVI =1;
+	SLAVERECE =0;
+	SPDR = dado[0];
+	for (int i=0;i<qtd;i++)
+	{
+		SLAVEDADOTX[i] =dado[i];
+	}
+	
+	SLAVEDADOIDTX =1;
+}
+
+//================================================================================
+
+void Master_Habilitar_spi(BITORDEM ORD, MODO MODE,CLOCK CLK)
+{
+	if (CLK > 0X03)
+	{
+		SPSR |= (1 << SPI2X);
+	}
+	SPCR = (1 << SPE)|(ORD)|(1 << MSTR)|(MODE)|(CLK);
+	DDRB |= SCK| MOSI | SS;
+	PORTB |= MISO;
+
+}
+//================================================================================
+unsigned char Master_envia_spi(unsigned char *dado, unsigned char qtd)
+{
+				
+	for (int i= 0; i<qtd;i++)
+	{
+		SPDR =dado[i];
+		while(!(SPSR &(1<<SPIF)) );
+	}
+	
+	return 0;
+}
+//================================================================================
+
+unsigned char Master_Recebe_spi(unsigned char *dado, unsigned char qtd)
+{
+	
+	
+	for (int i= 0; i<qtd;i++)
+	{
+		SPDR = 0xFF;
+		while(!(SPSR &(1<<SPIF)) );
+		dado[i] = SPDR;
+		
+	}
+	
+	
+	return 0;
+}
+//================================================================================
+
+void Slave_Habilitar_spi(BITORDEM ORD, MODO MODE)
+{
+	DDRB |= MISO;
+	PORTB |= SCK|MOSI|SS;
+	SPCR =(1<<SPE)|(1<<SPIE)| (ORD) | (MODE) ;
+	
+	PCICR |=(1<<PCIE0);
+	PCMSK0 |=(1<<PCINT2);
+	
+	sei();
+}
+
+
+//=================================================================================
+
+
+ISR(SPI_STC_vect)
+{
+	
+	
+	SLAVEDADORX[SLAVEDADOIDRX++] =SPDR;
+		
+	SPDR =SLAVEDADOTX[SLAVEDADOIDTX++];
+			
+}
+
+
+
+
+ISR(PCINT0_vect)
+{
+	if((PINB & SS)&&(SLAVERECE==1))
+	{
+		PORTC ^=(1<<5);
+		SLAVERECEBE(SLAVEDADORX,SLAVEDADOIDRX);
+		SLAVEDADOIDRX =0X00;
+	}
+	
+	if ((PINB & SS) && (SLAVEENVI==1))
+	{
+		SLAVEDADOIDTX =0x01;
+		SLAVERECE =1;
+		SLAVEENVI =0;
+	}
+	
+}
+
---
+++ SPI.h
@@ -0,0 +1,64 @@
+/*
+ * SPI.h
+ *
+ * Created: 09/08/2022 02:44:47
+ *  Author: Rodrigo
+ */ 
+
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+#define  F_CPU 16000000UL
+#include <util/delay.h>
+
+#define  SCK (1<<5)
+#define  MISO (1<<4)
+#define  MOSI (1<<3)
+#define  SS (1<<2)
+
+enum MODO
+{
+	  MODE0 = 0X00, //CPOL 0 CPHA 0
+	  MODE1 = 0X08, //CPOL 1 CPHA 0
+	  MODE2 = 0X0C, //CPOL 1 CPHA 1
+	  MODE3 = 0X04 //CPOL 0 CPHA 1
+};
+
+
+enum CLOCK
+{
+	  CLOCK0 = 0X00, // Fosc/4   SPR1 0 SPR0 0
+	  CLOCK1 = 0X01, // Fosc/16  SPR1 0 SPR0 1
+	  CLOCK2 = 0X02, // Fosc/64  SPR1 1 SPR0 0
+	  CLOCK3 = 0X03, // Fosc/128 SPR1 1 SPR0 1
+
+	  CLOCK4 = 0X04, // Fosc/2 SPI2X 1  SPR1 0 SPR0 0
+	  CLOCK5 = 0X05, // Fosc/8 SPI2X 1  SPR1 0 SPR0 1
+	  CLOCK6 = 0X06, // Fosc/32 SPI2X 1 SPR1 1 SPR0 0
+	  CLOCK7 = 0X07 // Fosc/64 SPI2X 1 SPR1 1 SPR0 1
+};
+
+
+
+enum BITORDEM
+{
+  LSB = 0X20,  //LSB PRIMEIRO DORD 1
+  MSB  =0X00   //MSB PRIMEIRO DORD 0
+};
+
+void Master_Habilitar_spi(BITORDEM ORD, MODO MODE,CLOCK CLK);
+unsigned char Master_envia_spi(unsigned char *dado, unsigned char qtd);
+unsigned char Master_Recebe_spi(unsigned char *dado, unsigned char qtd);
+
+
+void Slave_Habilitar_spi(BITORDEM ORD, MODO MODE);
+
+void Slave_RxTx_spi( void (*function)(unsigned char*, unsigned char) );
+void Slave_Envia_spi(const unsigned char *dado, unsigned char qtd);
+
+#endif /* SPI_H_ */
